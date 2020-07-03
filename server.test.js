fb2a433b6fc1f341c7332a83716ad305f177b4fa
Flávio Micheletti <flaviomicheletti@hotmail.com>
2020-07-03 15:14:00 -0300

    tests jest

--- server.test.js
+++ server.test.js
@@ -1,16 +1,71 @@
 const axios = require('axios');
 
-test(' GET /', async () => {
+axios.defaults.baseURL = 'http://localhost:3000';
 
-    const config = {
-        method: 'get',
-        url: 'http://localhost:3000/',
-        headers: {
-        }
-    };
+test('GET /', async () => {
 
-    let res = await axios.get(config.url);
+    let res = await axios.get('/');
     expect('oi').toBe(res.data);
 
 });
 
+    // a api lança uma exexeção não tratada, e agora José ?
+    test('GET /show', async () => {
+
+    try {
+        let res = await axios.get('/show');
+    } catch (error) {
+        expect(error.message).toBe('Request failed with status code 500');
+    }
+
+});
+
+test('GET /try-catch', () => {
+
+    //
+    // outra treta
+    //
+
+});
+
+test('GET /nothing', () => {
+
+    //
+    // + treta
+    //
+
+});
+
+test('GET /try-catch-log-e', async () => {
+
+    //
+    // + treta
+    //
+
+});
+
+test('GET /res-send', async () => {
+
+    let res = await axios.get('/res-send');
+    expect('').toBe(res.data);
+
+});
+
+test('GET /try-catch-send', async () => {
+
+    let res = await axios.get('/try-catch-send');
+    expect('Error: Oooops!').toBe(res.data);
+});
+
+test('GET /search', async () => {
+
+    let res = await axios.get('/search?params={"foo": "bar"}');
+    expect('Success').toBe(res.data);
+
+    try {
+        let res = await axios.get('/search?params={foo: bar}');
+    } catch (error) {
+        expect('Success').toBe(res.data);
+    }
+
+});
